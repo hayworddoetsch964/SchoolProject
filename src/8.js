042e24e823c708628ff7eadba767987ee50ca3fa
hayworddoetsch964 <hayworddoetsch964@gmail.com>
2025-03-13 19:53:17 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,11 @@
+function getRandomJSCode() {
+  let code = `
+    function add(a, b) {
+      return a + b;
+    }
+
+    console.log(add(2, 3)); // Output: 5`;
+  return code;
+}
+
+getRandomJSCode();
