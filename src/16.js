f2e4e3066b05ac0bdbee2cd8f70b3592b5a6ca50
hayworddoetsch964 <hayworddoetsch964@gmail.com>
2025-03-22 17:01:42 +0000

    Add 16.js

---
+++ src/16.js
@@ -0,0 +1,7 @@
+// Example of generating a simple array and accessing its elements
+let myArray = [1, 2, 3];
+console.log(myArray[0]); // Output: 1
+console.log(myArray[2]); // Output: 3
+
+myArray[2] = "newValue"; // Modify an element in the array
+console.log(myArray); // Output: [1, newValue, 3]
